fc7ce1ec5e181c5e195d2dafc44f661e667f21a7
Leyda <48352107+leydaaltun@users.noreply.github.com>
2021-01-09 15:30:48 +0300

    edited html

---
+++ index.js
@@ -0,0 +1,30 @@
+///const MainSlider = document.querySelector('.MainSlider')
+//MainSlider.addEventListener('click', Click)
+
+
+const capsuleFirst = document.querySelector('.capsule1')
+const capsuleSecond = document.querySelector('.capsule2')
+const firstSlide = document.querySelector('.carouselWrap1')
+const secondSlide = document.querySelector('.carouselWrap2')
+const slide = document.querySelector('.slide')
+const bucket = document.querySelector('.bucket')
+    //bucket.addEventListener('click', Click)
+
+/*function Click(event) {
+    console.log(event)
+    const clickedClassList = event.target.clickedClassList
+
+    if (document.getElementsByClassName('.capsule1').click()) {
+        slide.innerHTML = firstSlide
+    } else if (document.getElementsByClassName('.capsule2').click()) {
+        slide.innerHTML = secondSlide
+    }
+}*/
+
+/*bucket.addEventListener('click', function(event) {
+    console.log(event)
+    const clicked = event.target.clicked
+    if (clicked.d) {
+        slide.innerHTML = firstSlide
+    }
+})*/
